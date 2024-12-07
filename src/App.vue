59a2dd323c2af726fd88ac02581373dca3eca850
Sylvain <sylvain.brunet37@gmail.com>
2024-12-07 13:15:45 +0100

    Rename login

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <!-- src/App.vue -->
 <template>
-    <div id="app">
-      <FirestoreExample />
-      <AuthExample />
-    </div>
-  </template>
+  <div id="app">
+    <FirestoreExample />
+    <AuthExample />
+  </div>
+</template>
 
-	
 <script>
-import AuthExample from './components/AuthExample.vue';
+import AuthExample from './components/Login.vue'
 
 export default {
-name: 'App',
-components: {
-	AuthExample,
-},
-};
+  name: 'App',
+  components: {
+    AuthExample,
+  },
+}
 </script>
 
 <style>
 /* Global CSS */
-body, html {
+body,
+html {
   margin: 0;
   padding: 0;
   width: 100%;
@@ -35,5 +35,4 @@ body, html {
   align-items: center; /* Centrage vertical */
   min-height: 100vh; /* Occupe toute la hauteur de la fenêtre */
 }
-
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AuthExample from '@/components/AuthExample.vue'
+import AuthExample from '@/components/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('@/components/AuthExample.vue'),
+      component: () => import('@/components/Login.vue'),
     },
   ],
 })
