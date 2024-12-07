1e08268a9ecaf6f4d6c3359f0614aa8381ee89b5
Sylvain <sylvain.brunet37@gmail.com>
2024-12-07 15:27:20 +0100

    debug

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -42,7 +42,7 @@
 <script lang="ts">
 import { ref, computed } from 'vue'
 import { AuthStore } from '../stores/AuthStore'
-import { validateEmail, validatePassword } from '../utils/validator' // Import validators
+import { validateEmail, validatePassword } from '../utils/validator'
 
 export default {
   name: 'AuthForm',
