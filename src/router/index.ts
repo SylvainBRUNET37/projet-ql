2bb5e42b554551dfa5618192255377308374520c
Sylvain <sylvain.brunet37@gmail.com>
2024-12-07 14:05:35 +0100

    Delete home.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      meta: { requiresAuth: true },
-    },
     {
       path: '/login',
       name: 'login',
