a23f5cc0e7f1c1011359a169aa2d9143c0b1758b
Sylvain <sylvain.brunet37@gmail.com>
2024-12-07 14:14:07 +0100

    Fusion main.js et main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { db, auth } from './firebase.js'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.config.globalProperties.$db = db
+app.config.globalProperties.$auth = auth
 
 app.mount('#app')
