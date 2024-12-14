c32e4003f7190167f8a7a302a6b4697ebd3054d5
Sylvain <sylvain.brunet37@gmail.com>
2024-12-14 17:10:32 +0100

    Création fichiers store pour requete firebase

---
+++ src/stores/MaterialStore.ts
@@ -0,0 +1,5 @@
+/**
+ * @file MaterialStore.ts
+ *
+ * Gère les données des matériaux et les actions associées.
+ */
---
+++ src/stores/UserStore.ts
@@ -0,0 +1,5 @@
+/**
+ * @file UserStore.ts
+ *
+ * Gère les données utilisateur et les actions associées.
+ */
