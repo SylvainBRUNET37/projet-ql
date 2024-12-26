6748154ae9e8ca91abd376d36d56189b07daefa6
Sylvain <sylvain.brunet37@gmail.com>
2024-12-26 09:15:15 +0100

    Modifications sur les ref des equipements

--- src/stores/EquipmentStore.ts
+++ src/stores/EquipmentStore.ts
@@ -7,7 +7,16 @@
 import { defineStore } from 'pinia'
 import { ref, type Ref } from 'vue'
 import { db } from '../firebase'
-import { collection, getDocs, deleteDoc, doc, type DocumentData, addDoc } from 'firebase/firestore'
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  type DocumentData,
+  addDoc,
+  query,
+  where,
+} from 'firebase/firestore'
 import { FirebaseError } from 'firebase/app'
 
 /**
@@ -105,7 +114,18 @@ export const EquipmentStore = defineStore('equipment', () => {
    */
   const addEquipment = async (newEquipment: DocumentData): Promise<void> => {
     try {
-      alert(newEquipment)
+      let randomNumber: number
+      let uniqueRef: string
+
+      // Boucle pour générer une nouvelle référence tant qu'elle existe déjà dans Firestore
+      do {
+        randomNumber = Math.floor(Math.random() * 10000)
+        uniqueRef = `${newEquipment.ref}-${randomNumber}`
+      } while (await doesRefExist(uniqueRef))
+
+      // Met à jour l'equipement avec la nouvelle ref
+      newEquipment.ref = uniqueRef
+
       const equipmentRef = collection(db, 'equipments')
       await addDoc(equipmentRef, newEquipment)
       // Mettre à jour la liste locale après ajout
@@ -113,6 +133,19 @@ export const EquipmentStore = defineStore('equipment', () => {
     } catch (error) {
       console.error("Erreur lors de l'ajout de l'équipement :", error)
     }
+  }
+
+  /**
+   * Vérifie si une référence existe déjà dans Firestore.
+   *
+   * @param {string} ref - La référence à vérifier.
+   * @returns {Promise<boolean>} - Renvoie true si la référence existe déjà, sinon false.
+   */
+  const doesRefExist = async (ref: string): Promise<boolean> => {
+    const equipmentRef = collection(db, 'equipments')
+    const q = query(equipmentRef, where('ref', '==', ref))
+    const querySnapshot = await getDocs(q)
+    return !querySnapshot.empty
   }
 
   // Retourne les équipements, les erreurs et les fonctions
