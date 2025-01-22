9325e5c669ef29c54e81f8e058fc8d92092c658b
SylvainBRUNET37 <93929648+SylvainBRUNET37@users.noreply.github.com>
2025-01-22 14:14:05 +0100

    Rename cas de test equipement

--- src/tests/EquipmentResearch.spec.ts
+++ src/tests/EquipmentResearch.spec.ts
@@ -3,7 +3,6 @@ import { describe, it, expect, beforeEach, vi } from 'vitest'
 import EquipmentResearch from '@/components/equipmentResearch/Research.vue'
 import { EquipmentStore } from '@/stores/EquipmentStore'
 import { createPinia, setActivePinia } from 'pinia'
-import { VueWrapper } from '@vue/test-utils'
 
 // SC06 - Recherche d'un matériel
 describe('EquipmentResearch.vue', () => {
@@ -23,12 +22,21 @@ describe('EquipmentResearch.vue', () => {
   })
 
   // TC001
-  it('Rend correctement le composant', () => {
+  it('Consultation des matériels OK', () => {
     expect(wrapper.exists()).toBe(true)
+
+    // Vérifie que tous les équipements sont affichés
+    const equipmentCards = wrapper.findAll('.card')
+    expect(equipmentCards).toHaveLength(equipmentStore.equipment.length)
+
+    // Vérifie que chaque équipement est présent
+    equipmentStore.equipment.forEach((equipment, index) => {
+      expect(equipmentCards[index].text()).toContain(equipment.name)
+    })
   })
 
   // TC002
-  it('Recherche les équipements par nom', async () => {
+  it("Recherche d'un matériel avec la barre de recherche", async () => {
     // Remplir le champ de recherche et simuler la touche "Entrée"
     const searchInput = wrapper.find('input[type="text"]')
     await searchInput.setValue('Asus Laptop')
@@ -46,7 +54,7 @@ describe('EquipmentResearch.vue', () => {
   })
 
   // TC003
-  it("Affiche un message d'alerte si aucun équipement ne correspond à la recherche", async () => {
+  it("Recherche d'un matériel avec la barre de recherche mais aucun équipement ne correspond", async () => {
     const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => {}) // Mock alert
 
     // Recherche d'un équipement inexistant
@@ -60,7 +68,7 @@ describe('EquipmentResearch.vue', () => {
   })
 
   // TC004
-  it('Filtre les équipements par type lors du clic sur un tag', async () => {
+  it("Recherche d'un matériel avec un filtre", async () => {
     const toolsTag = wrapper.find('.tag')
     await toolsTag.trigger('click')
 
@@ -72,7 +80,7 @@ describe('EquipmentResearch.vue', () => {
   })
 
   // TC005
-  it('Réinitialise les filtres lors du clic sur le bouton Reset', async () => {
+  it('Le bouton pour reset les filtres reset bien les filtres', async () => {
     const resetButton = wrapper.find('.filter-container .button')
     await resetButton.trigger('click')
 
@@ -82,7 +90,7 @@ describe('EquipmentResearch.vue', () => {
   })
 
   // TC006
-  it('Affiche correctement tous les types uniques', () => {
+  it('Les filtres correspondent bien à tous les types des équipement disponibles', () => {
     const tags = wrapper.findAll('.tag')
 
     // 2 types doivent apparaitre : "Tools" et "Laptop"
