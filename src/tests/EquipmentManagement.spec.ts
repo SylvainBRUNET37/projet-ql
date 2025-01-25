91d6f567a2eb47045cd38df3f892017148cf6b44
Sylvain <sylvain.brunet37@gmail.com>
2025-01-25 16:19:15 +0100

    jsp

--- src/tests/EquipmentManagement.spec.ts
+++ src/tests/EquipmentManagement.spec.ts
@@ -1,11 +1,11 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect, beforeEach } from 'vitest'
-import UserManagement from '@/views/user/UserManagment.vue'
+import { EquipmentManagement } from '@/views/equipment/EquipmentManagement.vue'
 import { EquipmentStore } from '@/stores/EquipmentStore'
 import { createPinia, setActivePinia } from 'pinia'
 
 //Fichier qui est concerné par le test
-describe('UserManagement.vue', () => {
+describe('EquipmentManagement.vue', () => {
   //wrapper représente le component testé, authStore le système d'authentification
   let wrapper: any
   let equipmentStore: any
@@ -13,40 +13,8 @@ describe('UserManagement.vue', () => {
   //Avant chaque test, on initialise wrapper et authStore, Pinia est obligatoire pour bien initialiser les stores
   beforeEach(() => {
     setActivePinia(createPinia())
-    wrapper = mount(UserManagement)
+    wrapper = mount(EquipmentManagement)
     equipmentStore = EquipmentStore()
-  })
-
-  it('Modification d un matériel OK.', async () => {
-    const material = {
-      id: 1,
-      name: 'Old Name',
-      ref: 'Android',
-      type: 'Old Type',
-      description: 'Old Description',
-    }
-    equipmentStore.material = material
-
-    await wrapper.setProps({ material })
-    const nameInput = wrapper.find('#name')
-    const typeInput = wrapper.find('#type')
-    const refInput = wrapper.find('#ref')
-    const descriptionInput = wrapper.find('#description')
-    const saveButton = wrapper.find('button[type="submit"]')
-
-    await nameInput.setValue('Updated Name')
-    await typeInput.setValue('Updated Type')
-    await refInput.setValue('IOS')
-    await descriptionInput.setValue('Updated Description')
-    await saveButton.trigger('click')
-
-    expect(equipmentStore.updateMaterial).toHaveBeenCalledWith({
-      id: 1,
-      name: 'Updated Name',
-      ref: 'IOS',
-      type: 'Updated Type',
-      description: 'Updated Description',
-    })
   })
 
   it('Suppression d un matériel OK.', async () => {
