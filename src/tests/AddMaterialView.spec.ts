a8fdcaa59cf4ec9e6de98d29818c2223f3801ae4
Sylvain <sylvain.brunet37@gmail.com>
2025-01-26 15:29:19 +0100

    Commentaires

--- src/tests/AddMaterialView.spec.ts
+++ src/tests/AddMaterialView.spec.ts
@@ -4,84 +4,20 @@ import AddEquipment from '@/views/equipment/AddEquipment.vue'
 import { EquipmentStore } from '@/stores/EquipmentStore'
 import { createPinia, setActivePinia } from 'pinia'
 
-//Fichier qui est concerné par le test
+// SC05 - Ajout d'un matériel
 describe('AddEquipment.vue', () => {
-  //wrapper représente le component testé, authStore le système d'authentification
   let wrapper: any
   let equipmentStore: any
 
-  //Avant chaque test, on initialise wrapper et authStore, Pinia est obligatoire pour bien initialiser les stores
+  // Avant chaque test, on initialise le store et on monte le component
   beforeEach(() => {
     setActivePinia(createPinia())
     wrapper = mount(AddEquipment)
     equipmentStore = EquipmentStore()
-    equipmentStore.addEquipment = vi.fn() // Mock de la méthode addEquipment
+    equipmentStore.addEquipment = vi.fn()
   })
 
-  //Tous les tests qui utilisent le mot clé await doivent être async !
-  it('Un ou plusieurs champs ne sont pas remplis.', async () => {
-    const nameInput = wrapper.find('#name')
-    const typeInput = wrapper.find('#type')
-    const refInput = wrapper.find('#ref')
-    const descriptionInput = wrapper.find('#description')
-    const addEquipmentButton = wrapper.find('button[type="submit"]')
-
-    await nameInput.setValue('')
-    await typeInput.setValue('')
-    await refInput.setValue('')
-    await descriptionInput.setValue('')
-
-    await nameInput.trigger('blur')
-    await typeInput.trigger('blur')
-    await refInput.trigger('blur')
-    await descriptionInput.trigger('blur')
-
-    expect(wrapper.vm.errors.name).toBe('Please complete this field')
-    expect(wrapper.vm.errors.type).toBe('Please complete this field')
-    expect(wrapper.vm.errors.ref).toBe('Please complete this field')
-    expect(wrapper.vm.errors.description).toBe('Please complete this field')
-    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
-  })
-
-  it('Le nom n est pas valide.', async () => {
-    const nameInput = wrapper.find('#name')
-    const addEquipmentButton = wrapper.find('button[type="submit"]')
-
-    await nameInput.setValue('a!')
-    await nameInput.trigger('blur')
-
-    expect(wrapper.vm.errors.name).toBe('Invalid name')
-    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
-
-    await nameInput.setValue('a'.repeat(101))
-    await nameInput.trigger('blur')
-
-    expect(wrapper.vm.errors.name).toBe('Invalid name')
-    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
-  })
-
-  it('Le type n est pas valide.', async () => {
-    const typeInput = wrapper.find('#type')
-    const addEquipmentButton = wrapper.find('button[type="submit"]')
-
-    await typeInput.setValue('a!!!')
-    await typeInput.trigger('blur')
-
-    expect(wrapper.vm.errors.type).toBe('Invalid type')
-    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
-  })
-
-  it('La description n est pas valide.', async () => {
-    const descriptionInput = wrapper.find('#description')
-    const addEquipmentButton = wrapper.find('button[type="submit"]')
-
-    await descriptionInput.setValue('a'.repeat(201))
-    await descriptionInput.trigger('blur')
-
-    expect(wrapper.vm.errors.description).toBe('Invalid description')
-    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
-  })
-
+  // TC001 - Ajout d'un matériel OK
   it("Ajout d'un matériel OK", async () => {
     const nameInput = wrapper.find('#name')
     const typeInput = wrapper.find('#type')
@@ -90,7 +26,7 @@ describe('AddEquipment.vue', () => {
     const addEquipmentButton = wrapper.find('button[type="submit"]')
 
     // Mock pour simuler la méthode `addEquipment`
-    const addEquipmentMock = vi.spyOn(equipmentStore, 'addEquipment').mockImplementation(() => {
+    vi.spyOn(equipmentStore, 'addEquipment').mockImplementation(() => {
       return Promise.resolve()
     })
 
@@ -121,7 +57,83 @@ describe('AddEquipment.vue', () => {
     expect(wrapper.vm.$router.push).toHaveBeenCalledWith('/home')
   })
 
-  it("Vérifier si le bouton s'active correctement.", async () => {
+  //                       //
+  // TC002 Nnon applicable //
+  //                       //
+
+  // TC003 - Ajout d'un matériel avec un nom non conforme au REGEX
+  it("Ajout d'un matériel avec un nom non conforme au REGEX", async () => {
+    const nameInput = wrapper.find('#name')
+    const addEquipmentButton = wrapper.find('button[type="submit"]')
+
+    await nameInput.setValue('a!')
+    await nameInput.trigger('blur')
+
+    expect(wrapper.vm.errors.name).toBe('Invalid name')
+    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
+
+    await nameInput.setValue('a'.repeat(101))
+    await nameInput.trigger('blur')
+
+    expect(wrapper.vm.errors.name).toBe('Invalid name')
+    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
+  })
+
+  // TC004 - Ajout d'un matériel avec un type non conforme au REGEX
+  it("Ajout d'un matériel avec un type non conforme au REGEX", async () => {
+    const typeInput = wrapper.find('#type')
+    const addEquipmentButton = wrapper.find('button[type="submit"]')
+
+    await typeInput.setValue('a!!!')
+    await typeInput.trigger('blur')
+
+    expect(wrapper.vm.errors.type).toBe('Invalid type')
+    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
+  })
+
+  //                       //
+  // TC005 Nnon applicable //
+  //                       //
+
+  // TC006 - Ajout d'un matériel avec une description non conforme au REGEX
+  it("Ajout d'un matériel avec une description non conforme au REGEX", async () => {
+    const descriptionInput = wrapper.find('#description')
+    const addEquipmentButton = wrapper.find('button[type="submit"]')
+
+    await descriptionInput.setValue('a'.repeat(201))
+    await descriptionInput.trigger('blur')
+
+    expect(wrapper.vm.errors.description).toBe('Invalid description')
+    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
+  })
+
+  // TC007 - Ajout d'un matériel sans avoir rempli tous les champs
+  it("Ajout d'un matériel sans avoir rempli tous les champs", async () => {
+    const nameInput = wrapper.find('#name')
+    const typeInput = wrapper.find('#type')
+    const refInput = wrapper.find('#ref')
+    const descriptionInput = wrapper.find('#description')
+    const addEquipmentButton = wrapper.find('button[type="submit"]')
+
+    await nameInput.setValue('')
+    await typeInput.setValue('')
+    await refInput.setValue('')
+    await descriptionInput.setValue('')
+
+    await nameInput.trigger('blur')
+    await typeInput.trigger('blur')
+    await refInput.trigger('blur')
+    await descriptionInput.trigger('blur')
+
+    expect(wrapper.vm.errors.name).toBe('Please complete this field')
+    expect(wrapper.vm.errors.type).toBe('Please complete this field')
+    expect(wrapper.vm.errors.ref).toBe('Please complete this field')
+    expect(wrapper.vm.errors.description).toBe('Please complete this field')
+    expect(addEquipmentButton.attributes('disabled')).toBeDefined()
+  })
+
+  // TC008 - Vérifier si le bouton s'active correctement
+  it("Vérifier si le bouton s'active correctement", async () => {
     const nameInput = wrapper.find('#name')
     const typeInput = wrapper.find('#type')
     const refInput = wrapper.find('#ref')
